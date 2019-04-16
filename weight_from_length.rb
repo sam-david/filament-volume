c1711eb9f595acd09e42b059250172804c6b6222
Sam David <sfd1987@gmail.com>
2019-04-16 15:19:08 -0700

    clean up and output

--- weight_from_length.rb
+++ weight_from_length.rb
@@ -1,29 +1,28 @@
 input_length = ARGV.first
-input_diameter = ARGV[1]
-filament_type
-p input_length
-p input_diameter
+input_diameter = ARGV[1] || 1.75
+filament_type = ARGV[2] || "PLA"
+p "INPUT LENGTH: #{input_length}m, DIAMETER: #{input_diameter}, FILAMENT TYPE: #{filament_type}"
 
-volume_types = {
-  "PLA" => 1.24,
-  "ABS" => 1.04,
-  "ASA" => 1.07,
-  "PETG" => 1.27,
-  "Nylon" => 1.08,
-  "Polycarbonate" => 1.20,
-  "HIPS" => 1.07,
-  "PVA" => 1.19,
-  "TPU/TPE" => 1.20,
-  "PMMA" => 1.18,
-  "CopperFill" => 3.90
-}
 
 def filament_volume(length, diameter, filament_type)
-  diameter = 1.75 if diameter.nil?
+  volume_conversions_by_type = {
+    "PLA" => 1.24,
+    "ABS" => 1.04,
+    "ASA" => 1.07,
+    "PETG" => 1.27,
+    "Nylon" => 1.08,
+    "Polycarbonate" => 1.20,
+    "HIPS" => 1.07,
+    "PVA" => 1.19,
+    "TPU/TPE" => 1.20,
+    "PMMA" => 1.18,
+    "CopperFill" => 3.90
+  }
+
   filament_length = (length.to_f * 1000.0) # m to mm
   radius = diameter / 2
   volume = (Math::PI * radius ** 2 * filament_length) / 1000.0 # cubic mm to grams
-  volume * 1.24
+  volume * volume_conversions_by_type[filament_type]
 end
 
-p filament_volume(input_length, input_diameter)
+p filament_volume(input_length, input_diameter, filament_type)
